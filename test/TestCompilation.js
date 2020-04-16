e6318d3fcd4b22ad41288baab5d9cf0f8ed57de2
Derk Norton <derk.norton@gmail.com>
2020-04-16 17:34:13 -0600

    Restructured the source tree.

--- test/TestCompilation.js
+++ test/TestCompilation.js
@@ -9,8 +9,7 @@
  ************************************************************************/
 
 const debug = 1;
-const pfs = require('fs').promises;
-const crypto = require('crypto');
+const fs = require('fs');
 const mocha = require('mocha');
 const expect = require('chai').expect;
 const bali = require('bali-component-framework').api(1);
@@ -19,32 +18,34 @@ const compiler = require('bali-type-compiler').api(debug);
 
 describe('Bali Nebula™ Type Compilation', function() {
 
-    describe('Compile the core Bali type definitions.', async function() {
+    describe('Compile the core Bali type definitions.', function() {
 
-        const directories = await pfs.readdir('./src/bali/');
+        const folder = './src/bali/';
+        const directories = fs.readdirSync(folder);
         for (var i = 0; i < directories.length; i++) {
-            var directory = directories[i];
+            var type = directories[i];
+            var directory = folder + type + '/';
 
-            it('should compile the ' + directory, async function() {
-                const files = await pfs.readdir(directory);
+            it('should compile the ' + type + ' type', function() {
+                const files = fs.readdirSync(directory);
                 for (var i = 0; i < files.length; i++) {
                     var file = files[i];
 
                     // read in the source code
                     console.log('      ' + file);
                     var prefix = file.split('.').slice(0, 1);
-                    var filename = folder + prefix + '.bali';
-                    var source = await pfs.readFile(filename, 'utf8');
+                    var filename = directory + prefix + '.bali';
+                    var source = fs.readFileSync(filename, 'utf8');
                     var type = bali.component(source);
                     expect(type).to.exist;
 
                     // compile the source code
-                    await compiler.compileType(type);
+                    compiler.compileType(type);
 
                     // check for differences
                     source = type.toString() + '\n';  // POSIX compliant <EOL>
-                    await pfs.writeFile(filename, source, 'utf8');
-                    var expected = await pfs.readFile(filename, 'utf8');
+                    fs.writeFileSync(filename, source, 'utf8');
+                    var expected = fs.readFileSync(filename, 'utf8');
                     expect(expected).to.exist;
                     expect(source).to.equal(expected);
                 }
